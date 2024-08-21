058d436c80b12e8a2a7b212529dc3c5ff24f906a
Jo_00550337 <mandy900214@gmail.com>
2024-08-21 09:06:55 +0800

    JS評量第二次上傳

---
+++ JS/0821修改後(第二題發現新問題)/HashMap.js
@@ -0,0 +1,72 @@
+window.onload = function () {
+    const HashMap = function () {
+        let map = {};
+        return {
+            put: function (key, value) {
+                map[key] = value;
+            },
+            keys: function () {
+                const keySet = [];
+                for (let key in map) {
+                    keySet.push(key);
+                }
+                return keySet;
+            },
+            contains: function (key) {
+                for (let k in map) {
+                    if (k === key) {
+                        return true;
+                    }
+                }
+                return false;
+            },
+            get: function (key) {
+                return map[key];
+            },
+            clear: function () {
+                map = {};
+            }
+        };
+    };
+    const tagArticle = document.querySelector('article');
+    const dataMap = new HashMap();
+
+    document.getElementById("putBtn").addEventListener('click', function () {
+
+        const newKey = document.getElementById("key").value;
+        const newValue = document.getElementById("value").value;
+        //not Empty
+        if (!newKey) {
+            return;
+        }
+        //not repeated
+        if (dataMap.contains(newKey)) {
+            return;
+        }
+        dataMap.put(newKey, newValue);
+
+        tagArticle.innerHTML = '';
+        const tagP = document.createElement('p');
+        const tagBr = document.createElement('br');
+        tagP.innerHTML = '【RESULT】';
+        tagArticle.appendChild(tagP);
+        const keys = dataMap.keys();
+        let tagP1 = '';
+        for (let k of keys) {
+            tagP1 = document.createElement('p');
+            tagP1.innerHTML = '&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp' + k + '&nbsp:&nbsp' + dataMap.get(k);
+            tagArticle.appendChild(tagP1);
+        }
+
+    });
+
+    document.getElementById("clearBtn").addEventListener('click', function () {
+        dataMap.clear();
+        tagArticle.innerHTML = '';
+        document.getElementById("key").value = '';
+        document.getElementById("value").value = '';
+
+    });
+}
+
+
